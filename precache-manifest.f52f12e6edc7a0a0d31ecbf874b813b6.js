0cd85453ac4e209fcf9feca2c71e78a516fdcbc9
unknown <randkill.leader@gmail.com>
2020-09-06 19:56:40 +0700

    Updates

---
+++ precache-manifest.f52f12e6edc7a0a0d31ecbf874b813b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59d2780c482c24c363865e02171e57f0",
+    "url": "/color-tracking/index.html"
+  },
+  {
+    "revision": "1279f0df746045bee3da",
+    "url": "/color-tracking/static/css/main.6e97dd6d.chunk.css"
+  },
+  {
+    "revision": "1aeb4f607efddf88201a",
+    "url": "/color-tracking/static/js/2.561ec5ab.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/color-tracking/static/js/2.561ec5ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1279f0df746045bee3da",
+    "url": "/color-tracking/static/js/main.6902dd28.chunk.js"
+  },
+  {
+    "revision": "dd501770cfc0b85fcb6a",
+    "url": "/color-tracking/static/js/runtime-main.27aa508c.js"
+  }
+]);
